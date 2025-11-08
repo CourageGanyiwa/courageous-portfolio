a61232bf132d0a983142aad94077004aab9c2902
Courage Ganyiwa <couresma@gmail.com>
2025-11-08 18:35:16 +0200

    first push to repository

--- src/components/about/AboutClientSingle.jsx
+++ src/components/about/AboutClientSingle.jsx
@@ -1,13 +1,14 @@
 const AboutClientSingle = ({ title, image }) => {
-	return (
-		<>
-			<img
-				src={image}
-				className="w-64 py-5 px-10 border bg-secondary-light border-ternary-light dark:border-ternary-dark shadow-sm rounded-lg mb-8 cursor-pointer"
-				alt={title}
-			/>
-		</>
-	);
+  return (
+    <>
+      <img
+        src={image}
+        className="w-64 py-5 px-10 border bg-secondary-light border-ternary-light
+				 dark:border-ternary-dark shadow-sm rounded-lg mb-8 cursor-pointer"
+        alt={title}
+      />
+    </>
+  );
 };
 
 export default AboutClientSingle;
--- src/data/clientsData.js
+++ src/data/clientsData.js
@@ -1,54 +1,38 @@
 // Import images
-import AmazonImage from '../images/brands/amazon_gray.png';
-import SonyImage from '../images/brands/sony_gray.png';
-import AdidasImage from '../images/brands/adidas_gray.png';
-import FilaImage from '../images/brands/fila_gray.png';
-import NBImage from '../images/brands/nb_gray.png';
-import SamsungImage from '../images/brands/samsung_gray.png';
-import CanonImage from '../images/brands/canon_gray.png';
-import PumaImage from '../images/brands/puma_gray.png';
+import AmazonImage from "../images/brands/amazon_gray.png";
+import SonyImage from "../images/brands/sony_gray.png";
+import AdidasImage from "../images/brands/adidas_gray.png";
+import FilaImage from "../images/brands/fila_gray.png";
+import NBImage from "../images/brands/nb_gray.png";
+import SamsungImage from "../images/brands/samsung_gray.png";
+import CanonImage from "../images/brands/canon_gray.png";
+import PumaImage from "../images/brands/puma_gray.png";
+import Muscleandiron from "../images/brands/muscleandiron.png";
+import bladenutrition from "../images/brands/bladenutrition.png";
+import coresupplements from "../images/brands/coresupplements.webp";
+import pronutrtition from "../images/brands/pronutrition_brands.png";
 
-export const clientsHeading = 'Some of the brands that trust me';
+export const clientsHeading = "Some of the brands that trust me";
 
 export const clientsData = [
-	{
-		id: 1,
-		title: 'Amazon',
-		img: AmazonImage,
-	},
-	{
-		id: 2,
-		title: 'Sony',
-		img: SonyImage,
-	},
-	{
-		id: 3,
-		title: 'Adidas',
-		img: AdidasImage,
-	},
-	{
-		id: 4,
-		title: 'FILA',
-		img: FilaImage,
-	},
-	{
-		id: 5,
-		title: 'NB',
-		img: NBImage,
-	},
-	{
-		id: 6,
-		title: 'SAMSUNG',
-		img: SamsungImage,
-	},
-	{
-		id: 7,
-		title: 'CANON',
-		img: CanonImage,
-	},
-	{
-		id: 8,
-		title: 'PUMA',
-		img: PumaImage,
-	},
+  {
+    id: 1,
+    title: "Core Supplements",
+    img: coresupplements,
+  },
+  {
+    id: 2,
+    title: "Blade Nutrition",
+    img: bladenutrition,
+  },
+  {
+    id: 3,
+    title: "Muscle and Iron",
+    img: Muscleandiron,
+  },
+  {
+    id: 4,
+    title: "Pro Nutrition",
+    img: Muscleandiron,
+  },
 ];
